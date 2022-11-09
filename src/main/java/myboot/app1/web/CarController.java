93bac1bf6c41207353a93dcf4c60dd7450315496
Yanis Amrouche <54064644+yanisamrouche@users.noreply.github.com>
2022-11-09 11:00:52 +0100

    CarController.java

--- src/main/java/myboot/app1/web/CarController.java
+++ src/main/java/myboot/app1/web/CarController.java
@@ -1,7 +1,25 @@
 package myboot.app1.web;
 
-import org.springframework.web.bind.annotation.RestController;
+import myboot.app1.model.Car;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class CarController {
+
+    @GetMapping("/cars")
+    public ResponseEntity<List<Car>> getAllCars(){
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car());
+        return new ResponseEntity<>(cars, HttpStatus.OK);
+
+    }
+
+
+
 }
